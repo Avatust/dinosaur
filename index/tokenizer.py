169439610d5c4847d4300ee449316f1e071afb80
Avatust <martinroznovjak@gmail.com>
2017-10-16 23:20:32 +0300

    added comments

--- index/tokenizer.py
+++ index/tokenizer.py
@@ -1,7 +1,8 @@
 # the tokenize function will extract tokens
-import re
+import re #Regular Expressions
 
-regex = re.compile("[\w._-']+")
+regex = re.compile("[\w\d._-']+") # compiled regular expression to extract tokens
 
+# parses the text and returns an iterator of tokens in the given text
 def tokenize(text):
-  return regex.finditer(text)
+  return regex.finditer(text) 
