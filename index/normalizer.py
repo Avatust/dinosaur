a527966f2af56ffe6f40db165e68963281964532
Avatust <martinroznovjak@gmail.com>
2017-10-16 23:14:18 +0300

    Update normalizer.py

--- index/normalizer.py
+++ index/normalizer.py
@@ -1,8 +1,11 @@
 # normalize words for the index and querries
 
+table = str.maketrans(dict.fromkeys(".-_'")) # a transational table for the  normalization function, used to remove the respective characters
+
 # takes individual tokens/words and normalizes/prepares them for the index
 def normalize(token):
-  token = token.lower()
+  token = token.translate(table).lower() # remove dots, hyphens, underscores, apostrophes and put to lowercase
   # TODO:
   #   stemming
-  #   determining and normalising abbreviations (U.S.A. -> usa; End-of-sentence. -> end-of-sentence)
+  #   maybe a better care of special characters
+  return token
