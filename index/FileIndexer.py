86451de8ab4f6ba05f2b7a465eb10f89616f95dd
Avatust <martinroznovjak@gmail.com>
2017-10-17 11:04:09 +0300

    Update FileIndexer.py

--- index/FileIndexer.py
+++ index/FileIndexer.py
@@ -1,1 +1,22 @@
 
+class FileIndexer():
+  
+  _indexPath = ""
+  
+  def __init__(self, path_to_index):
+    self.loadIndex(path_to_index)
+  
+  def lookUp(self, ID):
+    return "filepath"
+  
+  def addEntry(self, word, fileID):
+    pass
+  
+  def loadIndex(self, path_to_index):
+    #load index to memory
+    self._indexPath = path_to_index
+    pass
+  
+  def saveIndex(self, path_to_index = None):
+    if (path_to_index == None):
+      path_to_index = _indexPath
