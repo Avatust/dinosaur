8484101f99365029e8740b0fb6c8cc9f774f6a11
Avatust <martinroznovjak@gmail.com>
2017-10-17 12:18:07 +0300

    simple implementation

--- index/KeyIndexer.py
+++ index/KeyIndexer.py
@@ -1,25 +1,41 @@
 # an interface to the index
 
+### TODO:
+  # error-proofness
+  # indent
+
+import pickle
+from os.path import isfile
+
 class KeyIndexer():
   
   _indexPath = ""
   
   def __init__(self, path_to_index):
+    self._index = dict()
     self.loadIndex(path_to_index)
   
   def lookUp(self, word):
-    return {"set of indexed files IDs"}
+    return self._index.get(word, set())   # returns the set of fileIDs or an empty set
   
   def addEntry(self, word, fileID):
-    pass
+    if word in self._index.keys():
+      self._index[word].add(fileID)
+     else:
+      self._index[word] = {fileID,}
   
   def loadIndex(self, path_to_index):
     #load index to memory
     self._indexPath = path_to_index
-    pass
+    if isfile(path_to_index):
+      with open(path_to_index) as file:
+        self._index = pickle.load(file)
   
   def saveIndex(self, path_to_index = None):
     if (path_to_index == None):
       path_to_index = _indexPath
+    with open(path_to_index, 'wb') as file:
+      pickle.dump(self._index, file)
+    
 
   
